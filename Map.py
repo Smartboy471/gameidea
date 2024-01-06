92d16caaf07ac97719b6da58a1dd0b2e4fb00b54
FegaMega <david.smidebrant@gmail.com>
2024-01-06 10:56:51 +0100

    s

--- Map.py
+++ Map.py
@@ -1,9 +1,9 @@
-import JH, Objects
+import JH, Objects, pygame
 
 class Map:
     def __init__(self) -> None:
         self.LevelPixelSize = [8192, 8192]
-        self.BackroundLevel = []
+        self.BackroundLevel = ["data/img/floor.png", "data/img/wall.png"]
         self.BackroundLevelsize = [256, 256]
         self.BackroundLevelPartSize = [32, 32]
         self.BackroundNames = [None, "data/img/floor.png", "data/img/wall.png"]
@@ -14,20 +14,20 @@ class Map:
     
     def setUpLevel(self, screenSize):
         #self.BackroundLevel
-        for y in range(0, self.BackroundLevelsize[0]):
-            self.BackroundLevel.append([])
-            for x in range(0, self.BackroundLevelsize[1]):
-                for i in range(0, 3):
-                    if i == self.JsonLevel[y][x]:
-                        if self.BackroundNames[i] != None:
-                            self.BackroundLevel[y].append(Objects.BackRoundPiece([x*self.BackroundLevelPartSize[0], y*self.BackroundLevelPartSize[1]], self.BackroundNames[i]))
+        #for y in range(0, self.BackroundLevelsize[0]):
+        #    self.BackroundLevel.append([])
+        #    for x in range(0, self.BackroundLevelsize[1]):
+        #        for i in range(0, 3):
+        #            if i == self.JsonLevel[y][x]:
+        #                if self.BackroundNames[i] != None:
+        #                    self.BackroundLevel[y].append(Objects.BackRoundPiece([x*self.BackroundLevelPartSize[0], y*self.BackroundLevelPartSize[1]], self.BackroundNames[i]))
 
 
         #Self.Level
         for y in range(0, int(self.LevelPixelSize[1]/self.chunkSize[1])):
             self.Level.append([])
             for x in range(0, int(self.LevelPixelSize[0]/self.chunkSize[0])):
-                self.Level[y].append([(self.BackroundLevel[2*y-1][2*x-1]), (self.BackroundLevel[2*y-1][2*x]), (self.BackroundLevel[2*y][2*x-1]), (self.BackroundLevel[2*y][2*x])])
+                self.Level[y].append([[[(self.JsonLevel[2*y-1][2*x-1]), (self.JsonLevel[2*y-1][2*x])], [(self.JsonLevel[2*y][2*x-1]), (self.JsonLevel[2*y][2*x])]]])
         #LoadedChunks
         left = 0
         right = int((screenSize[0] + (3*self.chunkSize[0])) / self.chunkSize[0])
@@ -45,11 +45,24 @@ class Map:
         return 0
     
     def draw(self, screen, shift):
-        for y in self.loadedChunks:
-            for x in y:
-                for part in x:
-
-                    part.draw(screen, shift)
+        surfaceFloor = pygame.image.load(self.BackroundLevel[0])
+        surfaceWall = pygame.image.load(self.BackroundLevel[1])
+        
+        for y in range(0, len(self.loadedChunks)):
+            for x in range(0, len(self.loadedChunks[y])):
+                for part in self.loadedChunks[y][x]:
+                    if type(part) == list:
+                        for loadedY in range(0, len(part)):
+                            
+                            for loadedX in range(0, len(part[loadedY])):
+                                if type(part[loadedY][loadedX]) == int:
+                                    if part == 0:
+                                        continue
+                                    if part == 1:
+                                        screen.blit(surfaceFloor, ((x*self.chunkSize[0])+(loadedX*self.BackroundLevelPartSize[0])-shift, 
+                                                                   (y*self.chunkSize[1])+(loadedY*self.BackroundLevelPartSize[1])-shift))
+                    else:
+                        part.draw(screen, shift)
         return 0
 
     def getCloseLevelparts(self, camera, screenSize):
